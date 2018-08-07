69baca11c9a3ec1b9d4b9caaf561d6f36e553202
vivek ramasamy <vivek.ramasamy@honeywell.com>
2018-08-07 16:46:45 +0530

    Bundling

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl() {
   // const inDevelopment =  window.location.hostname === 'localhost';
   // return inDevelopment ? 'http://localhost:3001/' : '/';
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+ return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
 }
 
 function getQueryStringParameterByName(name, url) {
